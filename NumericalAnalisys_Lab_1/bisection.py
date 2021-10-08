2e2489c36e874ed9218e63842ec5d32a7e23c003
DoctorMatt47 <DoctorMatt47@gmail.com>
2021-10-08 23:37:33 +0300

    Added bisection method

---
+++ NumericalAnalisys_Lab_1/bisection.py
@@ -0,0 +1,10 @@
+def execute(func):
+    eps = 0.00001
+    a = 0.0
+    b = 1.0
+    c = 0
+    while abs(a - b) > eps:
+        c = (a + b) / 2
+        if func(c) * func(a) < 0: b = c
+        else: a = c
+    return c
